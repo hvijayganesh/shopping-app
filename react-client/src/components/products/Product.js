25e2486cfb1fbed1fb4c22ad1bec445d9356902f
Vijay Ganesh H <vijay.g1@samsung.com>
2020-09-21 13:40:06 +0530

    update package-lock

--- react-client/src/components/products/Product.js
+++ react-client/src/components/products/Product.js
@@ -59,7 +59,7 @@ export class Product extends Component {
                   </div>
                   <div className="input-field col s6">
                     <button
-                      className="btn waves-effect waves-light"
+                      className="btn orange waves-effect waves-light"
                       type="submit"
                       name="action"
                       onClick={() => addToCart(product, this.state.quantity)}
